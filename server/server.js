4bd01f488df220dfa2fb847b6b170704f7ecdf3b
unknown <sanchit0841@gmmail.com>
2021-03-24 01:21:37 +0530

    before deploy

--- server/server.js
+++ server/server.js
@@ -1,49 +1,60 @@
-import express from 'express'
 import path from 'path'
-//import products from './data/products.js'
+import express from 'express'
+import dotenv from 'dotenv'
 import colors from 'colors'
 import morgan from 'morgan'
-import dotenv from 'dotenv'
+import { notFound, errorHandler } from './middlewares/errorMiddleware.js'
 import connectDB from './config/db.js'
-import {notFound,errorHandler} from './middlewares/errorHandler.js'
+
 import productRoutes from './routes/productRouter.js'
 import userRoutes from './routes/userRouter.js'
-import OrderRoutes from './routes/OrderRouter.js'
+import orderRoutes from './routes/OrderRouter.js'
 import uploadRoutes from './routes/uploadRoutes.js'
 
-const app = express();
-dotenv.config();
+dotenv.config()
+
 connectDB()
 
-if(process.env.NODE_ENV  === 'development'){
-    app.use(morgan('dev'))
-}
+const app = express()
 
-app.use((req,res,next) => {
-    console.log(req.originalUrl)
-    next();
-})
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
 
 app.use(express.json())
 
-app.get('/',(req,res) => {
-    res.send('aPI RUNNING')
-})
+app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
+app.use('/api/orders', orderRoutes)
+app.use('/api/upload', uploadRoutes)
 
+app.get('/api/config/paypal', (req, res) =>
+  res.send(process.env.PAYPAL_CLIENT_ID)
+)
 
-app.use('/api/products',productRoutes)
-app.use('/api/users',userRoutes)
-app.use('/api/orders',OrderRoutes)
-app.use('/api/upload',uploadRoutes)
-const __dirname = path.resolve() 
+const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
-app.get('/api/config/paypal', (req,res) => {
-    res.send(process.env.PAYPAL_CLIENT_ID)
-})
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/client/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
 
 app.use(notFound)
-
 app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
-app.listen(PORT,console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
+
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+)
